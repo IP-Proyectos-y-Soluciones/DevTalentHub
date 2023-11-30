c2d6613a0583e32b022c9be329299ee1cb4df867
Bladimir G. Parra B. | IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-11-29 22:02:00 -0500

    Database Configuration in the application

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const mongoose = require("mongoose");
+require("./config/db");
+
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+require("dotenv").config({ path: ".env" });
 
 const app = express();
 
@@ -15,9 +23,20 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+  }),
+);
 
 app.use("/", router());
 
-app.listen(5000);
+app.listen(process.env.PORT);
